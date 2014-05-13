b0047e4c0856e0849b8928d82c4dc90cc8dee84c
zakesbi15 <zakesbi15@smpanthers.com>
2014-05-13 10:56:09 -0700

    2f23f

---
+++ code.js
@@ -0,0 +1,7 @@
+function changeColor() {
+	$('#colorbutton').css("color", "purple");
+}
+
+function changeText() {
+	$("#quote").html('"How lucky I am to have something that makes saying goodbye so hard."');
+}
